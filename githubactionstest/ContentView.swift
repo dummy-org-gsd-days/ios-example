141b93a520e41eb6952bcece7b8f280b2104e342
Isaac <>
2019-10-11 10:36:46 +0200

    initial commit

---
+++ githubactionstest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  githubactionstest
+//
+//  Created by Isaac Lozano on 11/10/2019.
+//  Copyright © 2019 N26. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
